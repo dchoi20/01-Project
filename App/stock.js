877e7b09f60a6cf669c19d267539245a9b5cecb4
Danny Choi <choi.danny20@gmail.com>
2020-08-04 13:47:17 -0500

    updated the stock.js file. Added more information to the selected stock

--- App/stock.js
+++ App/stock.js
@@ -1,11 +1,17 @@
 let apiKey = "AIOFXIT69F29K1ID";
 
-$( document ).ready(function() {
+let today = new Date();
+let dd = today.getDate() - 1;
+let mm = today.getMonth() + 1;
+let yyyy = today.getFullYear();
+let todayDate = yyyy + "-" + "0" + mm + "-" + "0" + dd;
+
+$(document).ready(function () {
   let stockSelected = window.location.search.split("=")[1].toLocaleLowerCase();
 
   let queryUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSelected}&apikey=${apiKey}`;
 
-  console.log(queryUrl)
+  console.log(queryUrl);
   $.ajax({
     url: queryUrl,
     method: "GET",
@@ -14,9 +20,21 @@ $( document ).ready(function() {
     $(`<p>${response["Meta Data"]["2. Symbol"]}</p>`).appendTo("#stockInfo");
     $(
       `<p>Open: $${parseInt(
-        response["Time Series (Daily)"]["2020-08-03"]["1. open"]
-      ).toFixed(2)}</p>`
+        response["Time Series (Daily)"][todayDate]["1. open"]
+      ).toFixed(2)}</p>
+      <p>High: $${parseInt(
+        response["Time Series (Daily)"][todayDate]["2. high"]
+      ).toFixed(2)}</p>
+      <p>Low: $${parseInt(
+        response["Time Series (Daily)"][todayDate]["3. low"]
+      ).toFixed(2)}</p>
+      <p>Previous Close: $${parseInt(
+        response["Time Series (Daily)"][todayDate]["4. close"]
+      ).toFixed(2)}</p>
+      <p>Volume: $${parseInt(
+        response["Time Series (Daily)"][todayDate]["5. volume"]
+      ).toFixed()}</p>
+      `
     ).appendTo("#stockInfo");
   });
-
 });
